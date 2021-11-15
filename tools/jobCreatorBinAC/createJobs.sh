a81e9a42c33e62fdecc4f3350678f14fb63c6346
Johannes Martin <30448448+jammartin@users.noreply.github.com>
2021-11-15 23:58:25 +0100

    jobCreatorBinAC can create jobs with different thetas and more than one node without requesting all cores

--- tools/jobCreatorBinAC/createJobs.sh
+++ tools/jobCreatorBinAC/createJobs.sh
@@ -3,8 +3,9 @@
 # BEGIN CONFIGURATION
 SIM_DIR="../../simulations"
 modes=("Se" "Le" "Hi")
-lbIntervals=("10" "50" "100")
-numProcs=("10" "28" "56")
+lbIntervals=("1" "5" "10" "25" "50")
+numProcs=("2" "4" "8" "12" "16" "20" "28" "36")
+thetas=("0.5" "0.8")
 # END CONFIGURATION
 
 if [ -z "$1" ]
@@ -16,96 +17,111 @@ fi
 INIT_FILE_STEM=$1
 WORKDIR=$(pwd)
 
-# create neccessary directories
-mkdir -p $SIM_DIR/$INIT_FILE_STEM
-cd $SIM_DIR/$INIT_FILE_STEM
-mkdir -p config jobs
-
-echo "Creating configs and scripts in '$SIM_DIR/$INIT_FILE_STEM' ..."
-
-# creating submit script
-cp $WORKDIR/templates/submit.sh .
-echo "echo '... submitting jobs ...'" >> submit.sh
-
-# main loop
-for mode in "${modes[@]}"
+for theta in "${thetas[@]}"
 do
-    if [ "$mode" = "Se" ]; then
-	
-	JOBNAME=$INIT_FILE_STEM$mode
-	echo "Creating serial job '$JOBNAME'"
-	
-	# create configuration file
-	cfgPath=config/$JOBNAME.info
 
-	cp $WORKDIR/templates/config.info $cfgPath
-	
-	sed -i "s/INITFILE/$INIT_FILE_STEM.h5/g" $cfgPath
-	sed -i "s/OUTDIR/$JOBNAME/g" $cfgPath
-	sed -i "s/PARALLEL/false/g" $cfgPath
-	sed -i "s/HILBERT/false/g" $cfgPath
-	sed -i "s/LBINTERVAL/1/g" $cfgPath
+    # create neccessary directories
+    mkdir -p $SIM_DIR/${INIT_FILE_STEM}th${theta//./_}
+    cd $SIM_DIR/${INIT_FILE_STEM}th${theta//./_}
+    mkdir -p config jobs
 
-	#create job script
-	jobPath=jobs/$JOBNAME.sh
+    echo "Copying initial distribution file from $HOME if available."
+    cp ~/$INIT_FILE_STEM*.h5 $INIT_FILE_STEM.h5
 
-	cp $WORKDIR/templates/job.sh $jobPath
+    echo "Creating configs and scripts in '$SIM_DIR/${INIT_FILE_STEM}th${theta//./_}' ..."
 
-	sed -i "s/_JOBNAME_/$JOBNAME/g" $jobPath
-	sed -i "s/_NODES_/1/g" $jobPath
-	sed -i "s/_PPN_/1/g" $jobPath
+    # creating submit script
+    cp $WORKDIR/templates/submit.sh .
+    echo "echo '... submitting jobs ...'" >> submit.sh
 
-	# adding job to submit
-	echo "qsub jobs/$JOBNAME.sh" >> submit.sh
-    else
-	for lbInterval in "${lbIntervals[@]}"
-	do
-	    for numProc in "${numProcs[@]}"
+    # main loop
+    for mode in "${modes[@]}"
+    do
+	if [ "$mode" = "Se" ]; then
+	    
+	    JOBNAME=$INIT_FILE_STEM$mode
+	    echo "Creating serial job '$JOBNAME'"
+	    
+	    # create configuration file
+	    cfgPath=config/$JOBNAME.info
+
+	    cp $WORKDIR/templates/config.info $cfgPath
+	    
+	    sed -i "s/THETA/$theta/g" $cfgPath
+	    sed -i "s/INITFILE/$INIT_FILE_STEM.h5/g" $cfgPath
+	    sed -i "s/OUTDIR/$JOBNAME/g" $cfgPath
+	    sed -i "s/PARALLEL/false/g" $cfgPath
+	    sed -i "s/HILBERT/false/g" $cfgPath
+	    sed -i "s/LBINTERVAL/1/g" $cfgPath
+
+	    #create job script
+	    jobPath=jobs/$JOBNAME.sh
+
+	    cp $WORKDIR/templates/job.sh $jobPath
+
+	    sed -i "s/_JOBNAME_/$JOBNAME/g" $jobPath
+	    sed -i "s/_NODES_/1/g" $jobPath
+	    sed -i "s/_PPN_/1/g" $jobPath
+
+	    # adding job to submit
+	    echo "qsub jobs/$JOBNAME.sh" >> submit.sh
+	else
+	    for lbInterval in "${lbIntervals[@]}"
 	    do
-		JOBNAME="${INIT_FILE_STEM}lb${lbInterval}np$numProc$mode"
-		echo "Creating parallel job '$JOBNAME'"
+		for numProc in "${numProcs[@]}"
+		do
+		    JOBNAME="${INIT_FILE_STEM}lb${lbInterval}np$numProc$mode"
+		    echo "Creating parallel job '$JOBNAME'"
 
-		# create configuration file
-		cfgPath=config/$JOBNAME.info
+		    # create configuration file
+		    cfgPath=config/$JOBNAME.info
 
-		cp $WORKDIR/templates/config.info $cfgPath
-		
-		sed -i "s/INITFILE/$INIT_FILE_STEM.h5/g" $cfgPath
-		sed -i "s/OUTDIR/$JOBNAME/g" $cfgPath
-		sed -i "s/PARALLEL/true/g" $cfgPath
+		    cp $WORKDIR/templates/config.info $cfgPath
+		    
+		    sed -i "s/THETA/$theta/g" $cfgPath
+		    sed -i "s/INITFILE/$INIT_FILE_STEM.h5/g" $cfgPath
+		    sed -i "s/OUTDIR/$JOBNAME/g" $cfgPath
+		    sed -i "s/PARALLEL/true/g" $cfgPath
 
-		if [ "$mode" = "Hi" ]; then
-		    sed -i "s/HILBERT/true/g" $cfgPath
-		else
-		    sed -i "s/HILBERT/false/g" $cfgPath
-		fi
-		
-		sed -i "s/LBINTERVAL/$lbInterval/g" $cfgPath
+		    if [ "$mode" = "Hi" ]; then
+			sed -i "s/HILBERT/true/g" $cfgPath
+		    else
+			sed -i "s/HILBERT/false/g" $cfgPath
+		    fi
+		    
+		    sed -i "s/LBINTERVAL/$lbInterval/g" $cfgPath
 
-		#create job script
-		jobPath=jobs/$JOBNAME.sh
+		    #create job script
+		    jobPath=jobs/$JOBNAME.sh
 
-		cp $WORKDIR/templates/job.sh $jobPath
+		    cp $WORKDIR/templates/job.sh $jobPath
 
-		coresPerNode=28
-		let nodes=numProc/coresPerNode
+		    coresPerNode=28
+		    let nodes=numProc/coresPerNode
 
-		if [ "$nodes" -eq "0" ]; then
-		    nodes=1 # numProc < coresPerNode
-		    coresPerNode=$numProc
-		fi
-		
-		sed -i "s/_JOBNAME_/$JOBNAME/g" $jobPath
-		sed -i "s/_NODES_/$nodes/g" $jobPath
-		sed -i "s/_PPN_/$coresPerNode/g" $jobPath
+		    if [ "$nodes" -eq "0" ]; then
+			nodes=1 # numProc < coresPerNode
+			coresPerNode=$numProc
+		    else
+			let nodesRemainder=numProc%coresPerNode
+			if [ "$nodesRemainder" -gt "0" ]; then
+			    let nodes=nodes+1
+			    let coresPerNode=numProc/nodes
+			fi
+		    fi
+		    
+		    sed -i "s/_JOBNAME_/$JOBNAME/g" $jobPath
+		    sed -i "s/_NODES_/$nodes/g" $jobPath
+		    sed -i "s/_PPN_/$coresPerNode/g" $jobPath
 
-		# adding job to submit
-		echo "qsub jobs/$JOBNAME.sh" >> submit.sh
+		    # adding job to submit
+		    echo "qsub jobs/$JOBNAME.sh" >> submit.sh
+		done
 	    done
-	done
-    fi
-done
-echo "echo '... done.'" >> submit.sh
-chmod u+x submit.sh
-echo "... done."
-   
+	fi
+    done
+
+    echo "echo '... done.'" >> submit.sh
+    chmod u+x submit.sh
+    echo "... done."
+done   
